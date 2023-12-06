c2fc4b05c4fd4ba688388e39e6f14abcc1d6d3e9
Cyprian <Cyprian.Gibas@o2.pl>
2023-12-06 18:14:04 +0100

    Implemented abstract ViewModelBase.cs with INotifyPropertyChanged interface

--- ViewModel/LoginFormVM.cs
+++ ViewModel/LoginFormVM.cs
@@ -4,9 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GUI_Database_app.ViewModel
 {
-    class LoginFormVM
+    public class LoginFormVM : ViewModelBase
     {
+        private string _username;
+        private string _password;
+        private string _host;
+
+        private RelayCommand _loginCommand;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string Host
+        {
+            get => _host;
+            set
+            {
+                _host = value;
+                OnPropertyChanged(nameof(Host));
+            }
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return _loginCommand ?? (_loginCommand = new RelayCommand(param => ExecuteLogin(), param => CanExecuteLogin()));
+            }
+        }
     }
-}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace GUI_Database_app.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
