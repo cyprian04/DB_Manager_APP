227b1986189a8ae9df3f4efbfe68067111e3c0d4
Cyprian <Cyprian.Gibas@o2.pl>
2023-12-15 11:57:00 +0100

    changed the code layout in App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,22 +12,33 @@ namespace GUI_Database_app
 {
     public partial class App : Application
     {
+        private Container container;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            SetupContainer();
+            CreateAndShowLoginForm();
+        }
 
-            var container = new Container();
+        private void SetupContainer()
+        {
+            container = new Container();
             container.Register<ViewModel.LoginFormVM>(Lifestyle.Singleton);
             container.Register<ViewModel.MainWindowVM>(Lifestyle.Singleton);
             container.Register<Model.ICurrentUser, Model.CurrentUser>(Lifestyle.Singleton);
             container.Verify();
+        }
 
+        private void CreateAndShowLoginForm()
+        {
             var currentUser = container.GetInstance<Model.ICurrentUser>();
             var loginViewModel = container.GetInstance<ViewModel.LoginFormVM>();
             var mainViewModel = container.GetInstance<ViewModel.MainWindowVM>();
 
-            var loginForm = new LoginForm() {DataContext = loginViewModel};
+            var loginForm = new LoginForm() { DataContext = loginViewModel };
             loginForm.Show();
+
             loginForm.IsVisibleChanged += (s, ev) =>
             {
                 if (loginForm.IsLoaded && !loginForm.IsVisible)
