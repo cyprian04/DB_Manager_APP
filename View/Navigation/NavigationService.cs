70153efdc69823cbb2ca86a19db2a2b143ff5cb4
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-12-17 11:18:04 +0100

    Added class NavigationSerrive in folder Navigation

---
+++ View/Navigation/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using GUI_Database_app.ViewModel;
+
+namespace GUI_Database_app.View.Navigation
+{
+    public class NavigationService
+    {
+        private readonly Dictionary<string, Func<ViewModelBase>> viewModels;
+        private ViewModelBase currentViewModel;
+
+        public NavigationService()
+        {
+            viewModels = new Dictionary<string, Func<ViewModelBase>>();
+        }
+
+        public void RegisterViewModel(string key, Func<ViewModelBase> viewModelFactory)
+        {
+            if (!viewModels.ContainsKey(key))
+            {
+                viewModels.Add(key, viewModelFactory);
+            }
+        }
+
+        public void NavigateTo(string key)
+        {
+            if (viewModels.ContainsKey(key))
+            {
+                currentViewModel = viewModels[key].Invoke();
+                OnNavigationChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public ViewModelBase CurrentViewModel => currentViewModel;
+
+        public event EventHandler OnNavigationChanged;
+    }
+
+}
