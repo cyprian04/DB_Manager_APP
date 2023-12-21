b076d9ac8456cd5b09052b6d24499fce8aae41b5
Cyprian <Cyprian.Gibas@o2.pl>
2023-12-21 16:08:39 +0100

    Edited the whole menu layout and configurated it to handle dropdown Sidebar menu, works fine

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -10,27 +10,10 @@ namespace GUI_Database_app
     /// </summary>
     public partial class MainWindow : Window
     {
-       // Data.Connection CurrentUserConn = null;
-       // CustomControls.ViewControl View = null;
-       // CustomControls.MainInteractionControl MainInteraction = null;
-       // CustomControls.DbSettingsControl DbSettings = null;
-       // CustomControls.HomeControl Home = new CustomControls.HomeControl();
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        //public MainWindow(Data.Connection CurrentUserConn_in)
-        //{
-        //    InitializeComponent();
-        //    ContentArea.Content = Home;
-        //    CurrentUserConn = CurrentUserConn_in;
-        //
-        //    View = new CustomControls.ViewControl(CurrentUserConn);
-        //    MainInteraction = new CustomControls.MainInteractionControl(CurrentUserConn);
-        //    DbSettings = new CustomControls.DbSettingsControl(CurrentUserConn);
-        //}
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -55,27 +38,27 @@ namespace GUI_Database_app
             Application.Current.Shutdown();
         }
 
-        private void Btn_MouseMove(object sender, MouseEventArgs e)
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            var button = (Button)sender;
-            var pos = e.GetPosition(button);
-            var centerX = button.ActualWidth /2;
-            var centerY = button.ActualHeight /2;
-
-            var distance = Math.Sqrt(Math.Pow(pos.X - centerX, 2) + Math.Pow(pos.Y - centerY, 2)); 
-
-            if(distance <= 20)
-            {
-                button.Height = 45;
-                button.Width = 45;
-            }
+            Tg_Btn.IsChecked = false;
         }
 
-        private void Btn_MouseLeave(object sender, MouseEventArgs e)
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
-            var button = (Button)sender;
-            button.Height= 50;
-            button.Width = 50;
+            if (Tg_Btn.IsChecked == true)
+            {
+                tip_home.Visibility = Visibility.Collapsed;
+                tip_contacts.Visibility = Visibility.Collapsed;
+                tip_settings.Visibility = Visibility.Collapsed;
+                tip_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tip_home.Visibility = Visibility.Visible;
+                tip_contacts.Visibility = Visibility.Visible;
+                tip_settings.Visibility = Visibility.Visible;
+                tip_signout.Visibility = Visibility.Visible;
+            }
         }
     }
 }
